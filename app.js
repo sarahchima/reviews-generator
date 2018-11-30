448c253ea0457ed2681f5d09b65c8d2d5c15cf83
Sarah Chima <sarahchimao@gmail.com>
2018-11-30 11:20:46 +0100

    added node start to package.json

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const axios = require('axios');
 
 
 const app = express()
-const port = 3000
-
+const port = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
